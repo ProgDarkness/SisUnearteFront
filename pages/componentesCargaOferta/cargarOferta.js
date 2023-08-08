9e495fe4fff774b7d9a84ba3d93fca63a9237c91
ProgDarkness <gabrielmarcano141@gmail.com>
2023-08-07 23:35:14 -0400

    progreso de vista carga de oferta academica

--- pages/componentesCargaOferta/cargarOferta.js
+++ pages/componentesCargaOferta/cargarOferta.js
@@ -1,26 +1,171 @@
 import { Button } from 'primereact/button'
 import { Column } from 'primereact/column'
 import { DataTable } from 'primereact/datatable'
+import { Dialog } from 'primereact/dialog'
+import { Dropdown } from 'primereact/dropdown'
+import { InputText } from 'primereact/inputtext'
 import { useEffect, useState } from 'react'
 
 const CargarOferta = ({ cambioVista }) => {
   const [ofertas, setOfertas] = useState(null)
+  const [verOferta, setDialogVerOferta] = useState(false)
+  const [editarOferta, setDialogEditarOferta] = useState(false)
+  const [datosEditarOferta, setDatosEditarOferta] = useState(null)
+  const [opcionStatusOferta, setOpcionStatusOferta] = useState(null)
 
   useEffect(() => {
     setOfertas([
       {
         carrera: 'Artes Plasticas',
-        status_carrera: 'Ofertada',
+        status_carrera: 'Habilitada',
         cant_cupos: 100
       },
-      { carrera: 'Museologia', status_carrera: 'Ofertada', cant_cupos: 100 },
+      { carrera: 'Museologia', status_carrera: 'Habilitada', cant_cupos: 100 },
       {
         carrera: 'Dibujo Artistico',
         status_carrera: 'Deshabilitada',
         cant_cupos: 0
       }
     ])
+    setOpcionStatusOferta([{ name: 'Habilitada' }, { name: 'Desahabilitar' }])
   }, [])
+
+  const DialogVerOferta = () => {
+    return (
+      <Dialog
+        visible={verOferta}
+        onHide={() => setDialogVerOferta(false)}
+        header="Trayectos"
+        resizable={false}
+        draggable={false}
+      >
+        <div className="grid grid-cols-3 gap-4 m-2">
+          <span className="p-float-label field">
+            <InputText
+              className="w-full"
+              id="cod_carrera"
+              /* value={datosEstudiante?.cedula} */
+              autoComplete="off"
+            />
+            <label htmlFor="cod_carrera">Nombre de Trayecto</label>
+          </span>
+          <span className="p-float-label field">
+            <InputText
+              className="w-full"
+              id="cod_carrera"
+              /* value={datosEstudiante?.cedula} */
+              autoComplete="off"
+            />
+            <label htmlFor="cod_carrera">Nombre de Semestre</label>
+          </span>
+        </div>
+      </Dialog>
+    )
+  }
+
+  const DialogEditarOferta = () => {
+    return (
+      <Dialog
+        visible={editarOferta}
+        onHide={() => setDialogEditarOferta(false)}
+        header="Trayectos"
+        resizable={false}
+        draggable={false}
+      >
+        <div className="grid grid-cols-3 gap-4 m-2">
+          <span className="p-float-label field">
+            <InputText
+              className="w-full"
+              id="cod_carrera_ed"
+              value={datosEditarOferta?.carrera}
+              autoComplete="off"
+            />
+            <label htmlFor="cod_carrera_ed">Carrera</label>
+          </span>
+          <span className="p-float-label field">
+            <Dropdown
+              options={opcionStatusOferta}
+              className="w-full"
+              optionLabel="name"
+              optionValue="name"
+              id="status_oferta"
+              value={datosEditarOferta?.status_carrera}
+              onChange={(e) =>
+                setDatosEditarOferta({
+                  ...datosEditarOferta,
+                  status_carrera: e.value
+                })
+              }
+              autoComplete="off"
+            />
+            <label htmlFor="status_oferta">Estatus</label>
+          </span>
+          <span className="p-float-label field">
+            <InputText
+              className="w-full"
+              id="cant_cupos"
+              value={datosEditarOferta?.cant_cupos}
+              autoComplete="off"
+            />
+            <label htmlFor="cant_cupos">Cant. Cupos</label>
+          </span>
+          <div className="col-span-3 flex justify-center">
+            <Button
+              label="Modificar"
+              icon="pi pi-plus"
+              onClick={() => setDialogEditarOferta(false)}
+            />
+          </div>
+        </div>
+      </Dialog>
+    )
+  }
+
+  const accionBodyTemplate = (rowData) => {
+    return (
+      <div className="flex justify-center">
+        <Button
+          icon="pi pi-search"
+          className="p-button-info mr-1"
+          tooltip="Ver"
+          tooltipOptions={{ position: 'top' }}
+          onClick={() => setDialogVerOferta(true)}
+        />
+        <Button
+          icon="pi pi-pencil"
+          className="p-button-help mr-1"
+          tooltip="Modificar"
+          onClick={() => {
+            setDatosEditarOferta(rowData)
+            setDialogEditarOferta(true)
+          }}
+          tooltipOptions={{ position: 'top' }}
+        />
+        <Button
+          icon="pi pi-times"
+          className="p-button-danger"
+          tooltipOptions={{ position: 'top' }}
+        />
+      </div>
+    )
+  }
+
+  const bodyStatus = (rowData) => {
+    let colorTag = '#cdcdcd'
+    if (rowData.status_carrera === 'Habilitada') {
+      colorTag = '#84bf93'
+    } else {
+      colorTag = '#d56c6c'
+    }
+
+    return (
+      <div className="w-min rounded" style={{ backgroundColor: colorTag }}>
+        <h1 className="text-white text-sm font-semibold m-1">
+          {rowData.status_carrera.toUpperCase()}
+        </h1>
+      </div>
+    )
+  }
 
   return (
     <div className="grid grid-cols-5 gap-4 m-2 -mt-2">
@@ -46,15 +191,14 @@ const CargarOferta = ({ cambioVista }) => {
           }}
         />
       </div>
-      <h1 className="col-span-5 text-2xl font-semibold text-white ml-5 -mb-3">
-        Carreras
-      </h1>
+      <DialogVerOferta />
+      <DialogEditarOferta />
       <div className="col-span-5">
         <DataTable value={ofertas} emptyMessage="No hay carreras registradas.">
           <Column field="carrera" header="Carrera" />
-          <Column field="status_carrera" header="Estatus" />
+          <Column body={bodyStatus} header="Estatus" />
           <Column field="cant_cupos" header="Cant. Cupos" />
-          <Column />
+          <Column body={accionBodyTemplate} />
         </DataTable>
       </div>
     </div>
--- pages/inicio/cargaOfertaAcademica.js
+++ pages/inicio/cargaOfertaAcademica.js
@@ -18,6 +18,80 @@ const CargaOfertaAcademica = () => {
       {vistasInternas?.registrarCarrera && (
         <RegistroCarrera cambioVista={setVistasInternas} />
       )}
+      {/*  eslint-disable-next-line react/no-unknown-property */}
+      <style jsx global>{`
+        .p-datatable .p-datatable-thead > tr > th {
+          text-align: left;
+          padding: 0.75rem 0.75rem;
+          border: 1px solid #e4e4e4;
+          border-width: 0 0 1px 0;
+          font-weight: 500;
+          color: rgb(255 255 255);
+          background: #ae8e8e;
+          transition: none;
+        }
+
+        .p-datatable .p-datatable-tbody > tr {
+          background: #ae8e8e;
+          color: rgb(255 255 255 / 92%);
+          transition: none;
+        }
+
+        .p-dialog .p-dialog-content {
+          background: #ae8e8e;
+          color: rgb(255 255 255 / 87%);
+          padding: 0 1.25rem 1.25rem 1.25rem;
+        }
+
+        .p-dialog .p-dialog-header {
+          border-bottom: 0 none;
+          background: #ae8e8e;
+          color: rgb(255 254 254 / 87%);
+          padding: 1.25rem;
+          border-top-right-radius: 4px;
+          border-top-left-radius: 4px;
+        }
+
+        .p-float-label input:focus ~ label,
+        .p-float-label .p-inputwrapper-focus ~ label {
+          color: #ffffff;
+        }
+
+        .p-float-label input:focus ~ label,
+        .p-float-label input.p-filled ~ label,
+        .p-float-label textarea:focus ~ label,
+        .p-float-label textarea.p-filled ~ label,
+        .p-float-label .p-inputwrapper-focus ~ label,
+        .p-float-label .p-inputwrapper-filled ~ label {
+          top: -0.5rem !important;
+          border-radius: 1rem;
+          background-color: #3f51b5;
+          padding: 2px 4px;
+          margin-left: -4px;
+          margin-top: 0;
+        }
+
+        .p-float-label input:focus ~ label,
+        .p-float-label input.p-filled ~ label,
+        .p-float-label textarea:focus ~ label,
+        .p-float-label textarea.p-filled ~ label,
+        .p-float-label .p-inputwrapper-focus ~ label,
+        .p-float-label .p-inputwrapper-filled ~ label {
+          top: -0.5rem !important;
+          -webkit-border-radius: 1rem;
+          -moz-border-radius: 1rem;
+          border-radius: 1rem;
+          background-color: #3f51b5;
+          padding: 2px 4px;
+          margin-left: -4px;
+          margin-top: 0;
+          color: white;
+        }
+
+        .p-divider.p-divider-horizontal:before {
+          border-top: solid 1px rgb(255 255 255);
+        }
+      `}</style>
     </>
   )
 }
