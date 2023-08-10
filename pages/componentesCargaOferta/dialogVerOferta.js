69c8364ccd24348d504fe6bd200fe461995bf398
ProgDarkness <gabrielmarcano141@gmail.com>
2023-08-09 22:04:56 -0400

    avances de la vista de cargar oferta academica

--- pages/componentesCargaOferta/dialogVerOferta.js
+++ pages/componentesCargaOferta/dialogVerOferta.js
@@ -4,6 +4,7 @@ import { DataTable } from 'primereact/datatable'
 import { Dialog } from 'primereact/dialog'
 import { InputText } from 'primereact/inputtext'
 import { useEffect, useState } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
 
 const DialogVerOferta = ({
   activeDialogVerOferta,
@@ -12,6 +13,26 @@ const DialogVerOferta = ({
   setDatosVerOferta
 }) => {
   const [infoOferta, setInfoOferta] = useState(null)
+  const [infoHorario, setInfoHorario] = useState(null)
+  const [animacionVerHorario, setAnimacionVerHorario] = useState(false)
+
+  const animation = {
+    initial: {
+      x: 1700
+    },
+    animate: {
+      x: [1700, 0],
+      transition: {
+        duration: 1
+      }
+    },
+    exit: {
+      x: [0, -1700],
+      transition: {
+        duration: 0.2
+      }
+    }
+  }
 
   useEffect(() => {
     setInfoOferta([
@@ -25,18 +46,6 @@ const DialogVerOferta = ({
         trayecto: 'Trayecto 1',
         semestre: 'Semestre 1',
         materia: 'Fotografia',
-        profesor: 'Juan Manuel'
-      },
-      {
-        trayecto: 'Trayecto 1',
-        semestre: 'Semestre 1',
-        materia: 'Actuacion',
-        profesor: 'Juan Manuel'
-      },
-      {
-        trayecto: 'Trayecto 1',
-        semestre: 'Semestre 1',
-        materia: 'Teatro',
         profesor: 'Juan Manuel'
       },
       {
@@ -66,12 +75,50 @@ const DialogVerOferta = ({
       }
       /* ------------------------------------- */
     ])
+    setInfoHorario([
+      {
+        horas: '8:00-8:45',
+        lunes: '',
+        martes: 'Dibujo / Juan Manuel / Aula 3',
+        miercoles: '',
+        jueves: 'Fotografia / Juan Manuel / Aula 3',
+        viernes: '',
+        sabado: '',
+        domingo: ''
+      },
+      {
+        horas: '8:45-9:45',
+        lunes: '',
+        martes: 'Dibujo / Juan Manuel / Aula 3',
+        miercoles: '',
+        jueves: 'Fotografia / Juan Manuel / Aula 3',
+        viernes: '',
+        sabado: '',
+        domingo: ''
+      }
+    ])
   }, [])
 
   const actionBodyTemplate = () => {
     return (
       <div className="flex justify-center">
-        <Button label="Ver Horario" className="p-button-info" />
+        <Button
+          label="Ver Horario"
+          className="p-button-info"
+          onClick={() => setAnimacionVerHorario(!animacionVerHorario)}
+        />
+      </div>
+    )
+  }
+
+  const bodyDiaSemana = (rowData, dia) => {
+    const [materia, nombre, aula] = rowData[`${dia}`].split(' / ')
+
+    return (
+      <div className="text-center">
+        <p>{materia}</p>
+        <p>{nombre}</p>
+        <p>{aula}</p>
       </div>
     )
   }
@@ -83,6 +130,7 @@ const DialogVerOferta = ({
         setActiveDialogVerOferta(false)
         setDatosVerOferta(null)
       }}
+      style={{ height: '90%' }}
       header="Ver Oferta"
       resizable={false}
       draggable={false}
@@ -116,25 +164,101 @@ const DialogVerOferta = ({
           />
           <label htmlFor="cant_cupos">Cant. Cupos</label>
         </span>
+        <div className="flex justify-center align-middle">
+          {animacionVerHorario && (
+            <Button
+              label="Volver"
+              onClick={() => setAnimacionVerHorario(!animacionVerHorario)}
+              icon="pi pi-check"
+              iconPos="left"
+            />
+          )}
+        </div>
       </div>
-      <div className="col-span-5 mt-3">
-        <DataTable
-          value={infoOferta}
-          emptyMessage="No se encuentran trayectos registrados."
-          rowGroupMode="rowspan"
-          groupRowsBy={['trayecto', 'semestre']}
-        >
-          <Column field="trayecto" header="Trayectos" />
-          <Column field="semestre" header="Semestres" />
-          <Column field="materia" header="Materias" />
-          <Column field="profesor" header="Profesores" />
-          <Column
-            field="semestre"
-            body={actionBodyTemplate}
-            style={{ width: '8.16rem' }}
-          />
-        </DataTable>
-      </div>
+      <AnimatePresence initial={animacionVerHorario}>
+        {animacionVerHorario && (
+          <motion.div
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            variants={animation}
+          >
+            <div className="col-span-5 mt-3">
+              <DataTable
+                value={infoHorario}
+                emptyMessage="No existe Horario Registrado."
+                showGridlines
+              >
+                <Column field="horas" header="Horas" />
+                <Column
+                  field="lunes"
+                  header="Lunes"
+                  body={(rowData) => bodyDiaSemana(rowData, 'lunes')}
+                />
+                <Column
+                  field="martes"
+                  header="Martes"
+                  body={(rowData) => bodyDiaSemana(rowData, 'martes')}
+                />
+                <Column
+                  field="miercoles"
+                  header="Miercoles"
+                  body={(rowData) => bodyDiaSemana(rowData, 'miercoles')}
+                />
+                <Column
+                  field="jueves"
+                  header="Jueves"
+                  body={(rowData) => bodyDiaSemana(rowData, 'jueves')}
+                />
+                <Column
+                  field="viernes"
+                  header="Viernes"
+                  body={(rowData) => bodyDiaSemana(rowData, 'viernes')}
+                />
+                <Column
+                  field="sabado"
+                  header="Sabado"
+                  body={(rowData) => bodyDiaSemana(rowData, 'sabado')}
+                />
+                <Column
+                  field="domingo"
+                  header="Domingo"
+                  body={(rowData) => bodyDiaSemana(rowData, 'domingo')}
+                />
+              </DataTable>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+      <AnimatePresence initial={!animacionVerHorario}>
+        {!animacionVerHorario && (
+          <motion.div
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            variants={animation}
+          >
+            <div className="col-span-5 mt-3">
+              <DataTable
+                value={infoOferta}
+                emptyMessage="No se encuentran trayectos registrados."
+                rowGroupMode="rowspan"
+                groupRowsBy={['trayecto', 'semestre']}
+              >
+                <Column field="trayecto" header="Trayectos" />
+                <Column field="semestre" header="Semestres" />
+                <Column field="materia" header="Materias" />
+                <Column field="profesor" header="Profesores" />
+                <Column
+                  field="semestre"
+                  body={actionBodyTemplate}
+                  style={{ width: '8.16rem' }}
+                />
+              </DataTable>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
 
       {/* eslint-disable-next-line react/no-unknown-property */}
       <style jsx global>{`
