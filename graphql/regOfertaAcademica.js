7c05c50a6ee5b0e468698f4bfb25c050c894bd8f
Progdarkness <gabrielmarcano141@gmail.com>
2023-08-27 13:05:20 -0600

    agregando funciones para CRUD de periodo

---
+++ graphql/regOfertaAcademica.js
@@ -0,0 +1,71 @@
+import { gql } from 'graphql-request'
+
+export default {
+  GET_TODOS_PERIODOS: gql`
+    query obtenerPeriodos {
+      obtenerPeriodos {
+        status
+        message
+        type
+        response {
+          id
+          codigo
+          periodo
+          anio
+          mesi
+          mesf
+          semana
+          personal
+          mensaje
+          fei
+          fef
+          feacta
+          fedoc
+          fepregrado
+          femodificacion
+          feipre
+          fefpre
+          feinsc
+          fefinsc
+          feioferta
+          fefoferta
+          feiretiro
+          fefretiro
+          feinota
+          fefnota
+          estatus
+          trayecto
+        }
+      }
+    }
+  `,
+  SAVE_PERIODO: gql`
+    mutation crearPeriodo($InputPeriodo: InputPeriodo!) {
+      crearPeriodo(input: $InputPeriodo) {
+        status
+        message
+        type
+      }
+    }
+  `,
+  UPDATE_PERIODO: gql`
+    mutation actualizarPeriodo(
+      $InputActualizarPeriodo: InputActualizarPeriodo!
+    ) {
+      actualizarPeriodo(input: $InputActualizarPeriodo) {
+        status
+        message
+        type
+      }
+    }
+  `,
+  DELETE_PERIODO: gql`
+    mutation eliminarPeriodo($InputEliminarPeriodo: InputEliminarPeriodo!) {
+      eliminarPeriodo(input: $InputEliminarPeriodo) {
+        status
+        message
+        type
+      }
+    }
+  `
+}
