1f3ad40371e8cf4e0f36e64424b990f44182b7a4
chamohava <hildevalor@gmail.com>
2023-09-20 12:46:39 -0400

    Mejoras 2 postulacion

--- pages/inicio/inicio.js
+++ pages/inicio/inicio.js
@@ -12,7 +12,7 @@ import useSWR from 'swr'
 import { useSesion } from 'hooks/useSesion'
 
 const Inicio = () => {
-  const { idUser } = useSesion()
+  /* const { idUser } = useSesion()
   const [nacionalidad, setNacionalidad] = useState(null)
   const [cedula, setCedula] = useState('')
   const [primer_nombre, setPrimer_Nombre] = useState('')
@@ -41,9 +41,9 @@ const Inicio = () => {
       setCorreoElectronico(infoUser?.getInfoUsuario.response.correoElectronico)
       setCorreoElectronico(infoUser?.getInfoUsuario.response.correo_usuario)
     }
-  }, [infoUser])
+  }, [infoUser]) */
 
-  const accionBodyTemplate = (rowData) => {
+  /* const accionBodyTemplate = (rowData) => {
     return (
       <div className="flex justify-center">
         <Button
@@ -51,106 +51,31 @@ const Inicio = () => {
           className="p-button-help mr-1"
           tooltip="Inscribir"
           onClick={() => {
-            /* setDatosEditarPersonal(rowData)
-            setActiveDialogEditarPersonal(true) */
+            setDatosEditarPersonal(rowData)
+            setActiveDialogEditarPersonal(true)
           }}
           tooltipOptions={{ position: 'top' }}
         />
       </div>
     )
-  }
+  } */
 
   return (
     <div className="flex flex-col">
       <div className="overflow-x-auto relative shadow-md sm:rounded-lg background pt-5">
         <h1 className="text-3xl font-semibold text-white text-center mr-32 mb-6 -mt-3">
-          Datos Personales
+          Notas Informativas
         </h1>
 
         <div className="grid grid-cols-5 gap-4 mt-4 p-4">
-          <span className="p-float-label field">
-            <Dropdown
-              className="w-full"
-              id="nacionalidad"
-              options={tiposNacionalidad?.obtenerNacionalidades.response}
-              value={nacionalidad}
-              onChange={(e) => {
-                setNacionalidad(e.value)
-              }}
-              optionLabel="código"
-              disabled
-            />
-            <label htmlFor="nacionalidad">Nacionalidad</label>
-          </span>
-          <span className="p-float-label field">
-            <InputText
-              keyfilter={'num'}
-              maxLength={9}
-              className="w-full"
-              id="cedula"
-              value={cedula}
-              onChange={(e) => setCedula(e.target.value)}
-              autoComplete="off"
-              disabled
-            />
-            <label htmlFor="cedula">Cédula</label>
-          </span>
-          <span className="p-float-label field">
-            <InputText
-              className="w-full"
-              id="primer_Nombre"
-              value={primer_nombre}
-              onChange={(e) => setPrimer_Nombre(e.target.value.toUpperCase())}
-              autoComplete="off"
-              disabled
-            />
-            <label htmlFor="primer_nombre">Primer Nombre</label>
-          </span>
-          <span className="p-float-label field">
-            <InputText
-              className="w-full"
-              id="primer_apellido"
-              value={primer_apellido}
-              onChange={(e) => setPrimer_Apellido(e.target.value.toUpperCase())}
-              autoComplete="off"
-              disabled
-            />
-            <label htmlFor="primer_apellido">Primer Apellido</label>
-          </span>
-          <span className="p-float-label field">
-            <InputText
-              className="w-full"
-              id="correoElectronico"
-              value={correoElectronico}
-              onChange={(e) => setCorreoElectronico(e.target.value)}
-              autoComplete="off"
-              disabled
-            />
-            <label htmlFor="correoElectronico">Correo Electronico</label>
-          </span>
+          <p>0</p>
         </div>
       </div>
       <Divider className="col-span-5" />
       <div>
         <div className="flex flex-row">
           <div className="tab-content justify-center rounded-lg shadow-md p-2 w-3/5">
-            <div className="w-full text-center col-span-5">
-              <h1 className="text-3xl font-semibold text-white text-center mr-10 mb-0 -mt-0">
-                Postulaciones Activas
-              </h1>
-            </div>
-            <div className="col-span-5">
-              <DataTable
-                value={infoPostuUsu?.obtenerPostulacionUsuario.response}
-                emptyMessage="No se encuentran trayectos registrados."
-              >
-                <Column field="periodo" header="Periodo" />
-                <Column field="carrera" header="Carrera" />
-                <Column field="sede" header="Sede" />
-                <Column field="estatus" header="Estatus" />
-                <Column body={accionBodyTemplate} />
-              </DataTable>
-            </div>
+            <p>1</p>
           </div>
 
           <div className="tab-content justify-center rounded-lg shadow-md py-1 px-3 bg-gray-200 w-2/5">
